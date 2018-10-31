7a2e81a8288c0fb0a8731aa329dde2507d0c4a9a
fauzan_bintang <obin.aji@gmail.com>
2018-10-31 17:16:36 +0700

    array-of-objects done

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ createObj('Akbar', 1, 'male');
 createObj('icha', 1, 'female')
 
 console.log(arr);
+
+function getData(name) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === name) {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+console.log(getData('icha'));
