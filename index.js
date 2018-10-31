87eeab00a6040aee1c06b5ff4bb1325048531bb8
Desy Armariena <desy.armariena@gmail.com>
2018-10-31 11:26:36 +0700

    Release 1: Add function getData

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ function createObj(name, phase, gender) {
     arr.push(newObj);
 }
 
+function getData(name) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === name) {
+            return arr[i];
+        }
+    }
+
+    return `There is no student with name '${name}' in our database`;
+}
+
 createObj('Akbar', 1, 'male');
 createObj('Icha', 1, 'female');
 createObj('Adhit', 2, 'male');
@@ -12,3 +23,5 @@ createObj('Tama', 2, 'male');
 createObj('Rifky', 3, 'male');
 
 console.log(arr);
+console.log(getData('Icha'));
+console.log(getData('Desy'));
